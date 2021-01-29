53912669436d3b9cdf3eb52131555b556495e0d3
Andreas Tscheinig <and.tscheinig@gmail.com>
2021-01-29 10:25:11 +0100

    add component to fetch user name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PowerUpContainerComponent } from './power-up-container/power-up-container.component';
+import { UsernameInputComponent } from './username-input/username-input.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PowerUpContainerComponent
+    PowerUpContainerComponent,
+    UsernameInputComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/username-input/username-input.component.ts
@@ -0,0 +1,15 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-username-input',
+  templateUrl: './username-input.component.html',
+  styleUrls: ['./username-input.component.scss']
+})
+export class UsernameInputComponent {
+
+  userName = '';
+
+  addUserName(event): void {
+    debugger;
+  }
+}
